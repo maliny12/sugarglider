9d1418346fbb3f9d120d768476823a720f39e3a7
Maliny Po <po0007@student.monash.edu>
2024-08-14 23:39:26 +1000

    Add ggthemes to suggest

--- data-raw/rain.R
+++ data-raw/rain.R
@@ -1,5 +1,7 @@
 ## code to prepare `rain` dataset goes here
 
-stations <- get_stations_metadata(which_api = "DPIRD")
+stations <- get_stations_metadata(which_api = "DPIRD") |>
+  dplyr::filter_at(vars(latitude, longitude),
+            all_vars(!is.na(.)))
 
 usethis::use_data(rain, overwrite = TRUE)
